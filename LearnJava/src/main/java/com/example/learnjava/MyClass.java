bef8c3101ca26e53d723351ecd994b2f4d3f79f6
athmekr <thanosmkr@gmai.com>
2020-05-08 13:48:22 +0300

    Add course material and fixed spacing

---
+++ LearnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -0,0 +1,10 @@
+package com.example.learnjava;
+
+public class MyClass {
+    public static void main(String[] args) {
+
+        String name = "Thanos";
+        System.out.println("Hello world!");
+        System.out.println(name);
+    }
+}
